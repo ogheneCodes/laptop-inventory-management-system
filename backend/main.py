727550a43285d0c88d2063b5973e04ecf0569555
OgheneCodes <oghenemaga.ebobo@unn.edu.ng>
2026-07-31 22:13:34 +0000

    Connect FastAPI to SQLite and retrieve laptops

--- backend/main.py
+++ backend/main.py
@@ -1,52 +1,30 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
-# Mock inventory data
-laptops = [
-    {
-        "id": 1,
-        "brand": "HP",
-        "model": "EliteBook 840 G8",
-        "processor": "Intel Core i5",
-        "ram": "16GB",
-        "storage": "512GB SSD",
-        "price": 350000,
-        "status": "In Stock"
-    },
-    {
-        "id": 2,
-        "brand": "Dell",
-        "model": "Latitude 5420",
-        "processor": "Intel Core i5",
-        "ram": "8GB",
-        "storage": "256GB SSD",
-        "price": 280000,
-        "status": "Sold",
-    },
-
-
-{
-	"id": 3,
-        "brand": "Lenovo",
-        "model": "ThinkPad T14",
-        "processor": "Intel Core i5",
-        "ram": "16GB",
-        "storage": "1TB SSD",
-        "price": 300000,
-        "status": "Available",
-},
-
-]
+DATABASE = "backend/database/lims.db"
 
 
 @app.get("/")
 def home():
     return {
-        "Welcome to Oghenemaga Signature LIMS"
+        "message": "Welcome to Oghenemaga Signature LIMS"
     }
 
 
 @app.get("/laptops")
 def get_laptops():
-    return laptops
+    conn = sqlite3.connect(DATABASE)
+
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM laptops")
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return [dict(row) for row in rows]
